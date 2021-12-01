1cf0b5c4addff4d7550a2dd581c62e420a894bc6
IVAN19717 <89376848+IVAN19717@users.noreply.github.com>
2021-12-01 13:47:07 +0200

    Update start-bitcore.sh

--- start-bitcore.sh
+++ start-bitcore.sh
@@ -4,4 +4,4 @@ while true
 do
 ./bit-main/wildrig-multi -a megabtx  -o stratum+tcp://stratum-eu.rplant.xyz:7066 -u sV4pk7piZ3BGSFeQWL46DC7CpVpSZzUQqN  --opencl-launch auto --print-full --print-power
 sleep 5
-done
+
