ab7c78ed7583c3c6bdb796752f1e2346fae66966
IVAN19717 <89376848+IVAN19717@users.noreply.github.com>
2021-12-01 23:53:46 +0200

    Update start-bitcore.sh

--- start-bitcore.sh
+++ start-bitcore.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-!while [ 1 ]; 
-!do
-  ! ./bit-main/wildrig-multi --a megabtx  --o stratum+tcp://stratum-eu.rplant.xyz:7066 --u sV4pk7piZ3BGSFeQWL46DC7CpVpSZzUQqN
- !sleep 5
-!done
-
+! ./bit-main/wildrig-multi --algo megabtx  --o stratum+tcp://stratum-eu.rplant.xyz:7066 --u sV4pk7piZ3BGSFeQWL46DC7CpVpSZzUQqN
+ 
 
 	
 	
