a89edb72c5ecfc73204c6e4988fa8f4b8f12c5a6
IVAN19717 <89376848+IVAN19717@users.noreply.github.com>
2021-12-01 12:41:57 +0200

    Add files via upload

---
+++ start-bitcore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+./wildrig-multi --print-full --algo megabtx --url stratum+tcp://stratum-eu.rplant.xyz:7066 --user sV4pk7piZ3BGSFeQWL46DC7CpVpSZzUQqN --pass x
+sleep 5
+done
